dfc6753b95ec5b5ff38a0b95340b27881fdbd5da
Steve Faulkner <southpolesteve@gmail.com>
2013-07-06 23:30:06 -0500

    Add ember varient to production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,8 @@ EmberIssues::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.ember.variant = :production
+
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
